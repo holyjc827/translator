6df17182d059419ba6c5feb98cbe29774a063237
holyjc827 <holyjc827@gmail.com>
2020-11-27 22:07:23 +0100

    Function to convert 'int'

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ file = open("test.cpp","r")
 for line in file:
     if search("printf", line):
         s = line.split("(")[1].split(")")
-        print(s)
-        f = open("text.py","w")
-        f.write("print("+s[0]+")")
+        with open('text.py', 'a') as l:
+            l.writelines("%s\n" % ("print("+s[0]+")"))
+    if search(r'\bint\b', line): #word boundary to check the exact word 'int'
+        if search("=", line):
+            f = line.split("=")
+            name = f[0].split(" ")[-1]
+            assignment = f[1].split(";")[0]
+            with open('text.py', 'a') as l:
+                l.writelines("%s\n" % (name + " = int(" + assignment + ")"))
+        elif search(";\n", line):
+            f = line.split(" ")[-1].strip(";\n")
+            with open('text.py', 'a') as l:
+                l.writelines("%s\n" % (f+ " = ' '"))
